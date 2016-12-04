44f2ca91cfbe7bb6424664da252c4041d4d6853f
zm <maratn566ek@gmail.com>
2016-12-04 23:25:39 +0300

    form upgrade

--- index.js
+++ index.js
@@ -17,11 +17,26 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.use(require('body-parser').urlencoded({extended:true}));
+
 // POST method route
 app.post('/', function (req, res) {
 	console.log('Post request!!!');
-	console.log(req);
-	console.log(res);
-  res.send('POST request to the homepage');
+	// console.log(req);
+	// console.log(res);
+	// console.log(req.sity);
+	if (req.xhr || req.accepts('json,html') === 'json') {
+		console.log('success');
+		console.log('Form' + req.query.form);
+		console.log('  ' + req.body.name);
+		console.log('  ' + req.body.city);
+		// res.send({success:true});
+	} else {
+		console.log('redirect');
+		res.redirect(303, '/thank-you');
+	}
+	res.redirect(303, '/footer');
+	// res.redirect(303, '/thank-you.html');
+  // res.send('POST request to the homepage');
 });
 
